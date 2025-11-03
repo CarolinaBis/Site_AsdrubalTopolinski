b087b9e44fe5464db490900885fef686981561da
Carolina Bis <carolbis09@gmail.com>
2025-11-03 16:37:56 -0300

    Initialize project structure and add main components

---
+++ src/Routes.jsx
@@ -0,0 +1,28 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Inicio from "./paginas/Inicio";
+import SobreMim from "./paginas/SobreMim";
+import Menu from "./componentes/Menu";
+import Rodape from "./componentes/Rodape";
+import PaginaPadrao from "./componentes/PaginaPadrao";
+import Postagem from "./paginas/Postagem";
+import NaoEncontrada from "./paginas/NaoEncontrada";
+
+function AppRoutes() {
+  return (
+    <BrowserRouter>
+      {/* Menu fora de todas as rotas, p/ aparecer em todas as páginas */}
+      <Menu />
+      <Routes>
+        <Route path="/" element={<PaginaPadrao />}>
+          <Route path="/" element={<Inicio />} />
+          <Route path="/sobremim" element={<SobreMim />} />
+          <Route path="/posts/:id" element={<Postagem />} />
+        </Route>
+        <Route path="*" element={<NaoEncontrada/>} />
+      </Routes>
+      {/* Rodape fora de todas as rotas, p/ aparecer em todas as páginas */}
+      <Rodape />
+    </BrowserRouter>
+  );
+}
+export default AppRoutes;
---
+++ src/paginas/Inicio/index.jsx
@@ -0,0 +1,20 @@
+import Post from "@/componentes/Post";
+import styles from "./Inicio.module.css";
+import posts from "@/json/posts.json";
+
+export default function Inicio() {
+  // Apenas o conteúdo principal da página Inicio,
+  // isto é, os posts !!
+
+  return (
+    <ul className={styles.posts}>
+      {posts.map((post) => {
+        return (
+          <li key={post.id}>
+            <Post post={post} />
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
